7e9521e7741354a15717a0cb8bc8ad9125eeb8b9
sherlocksameer7 <arijitsameers@gmail.com>
2022-03-05 11:48:54 +0530

    Changes has been added

--- methods_tuple.py
+++ methods_tuple.py
@@ -1,16 +1,16 @@
 tuple = (1, 3, 7, 8, 7, 5, 4, 6, 8, 5)
 x = tuple.count(5)
 print(x)
-# it allows a duplicates and count it using this method.
-
+# # it allows a duplicates and count it using this method.
+#
 tuple = ("Sameer", "Dhanush", "Akash", "Pradeep", "Rilwan", "Sameer")
 name = tuple.count("Sameer")
 print(name)
 # this determines the same string value that can be counted through this method.
 
 tuple = (1, 3, 7, 8, 7, 5, 4, 6, 8, 5)
-x = tuple.index(7)
-print(x)
+x = tuple[0]
+print(x)  # for finding a value through index of giving an index values.
 # this determines the finding of a value that can be included in the tuple
 # using of index( any value of an input ) to get the position of that index value that we given.
 # in this duplicates are allowed but, it shows the first position to the display with any kind of duplications.
